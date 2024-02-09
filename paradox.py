f5bf98161b240859a57e51ea7b2ecf6957630a89
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2024-02-08 21:56:50 -0800

    Finished creating the intro for the paradox. Now it is time to get to the real code. :)

---
+++ paradox.py
@@ -0,0 +1,15 @@
+import random
+import datetime
+import math
+
+# include the website portfolio link here when ready.
+website_url = ""
+
+def printIntro():
+    print("\nBirthday Paradox, by Nithin Rajesh " + website_url + "\n\nThe Birthday Paradox shows us that in a group of N people, the odds that two of them have matching birthday is surprisingly large. This program does a Monte Carlo simulation (that is, repeated random simulations) to explore this concept.\n\nIt's not actually a paradox, it's just a surprising result.\n\nHow many birthdays shall I generate (Max 100):\n")
+
+def generateRandomBirthday():
+    # Generate a random birthday using the datetime module.
+    pass
+
+printIntro()
